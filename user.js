304a16b6e7e656be3f6de738be9edcd9f2b33261
Przemek <vatier@gazeta.pl>
2021-03-21 18:02:02 +0100

    trim user token

--- user.js
+++ user.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const config = require('./config');
 
 exports.token = function () {
-  return fs.readFileSync('./data/user_token', {encoding:'utf8', flag:'r'});
+  return fs.readFileSync('./data/user_token', {encoding:'utf8', flag:'r'}).trim();
 }
 
 exports.id = function () {
