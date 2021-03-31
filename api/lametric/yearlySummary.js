5dbe5fcf16f595fadce881909ad8fe35325d4f1e
Przemek <vatier@gazeta.pl>
2021-03-31 20:40:03 +0200

    divide month to three parts

--- api/lametric/yearlySummary.js
+++ api/lametric/yearlySummary.js
@@ -2,14 +2,20 @@ const config = require('../../config');
 
 module.exports = function (trainings) {
   const distancePerHalfMonth = Array(37).fill(0);
-  distancePerHalfMonth[36] = Math.round(config.GOAL_DISTANCE / 12 / 2);
+  distancePerHalfMonth[36] = Math.round(config.GOAL_DISTANCE / 12 / 3);
   trainings.forEach(training => {
     const date = new Date(training.date);
-    const basket = (date.getMonth() * 3) + (date.getDate() > 14 ? 1 : 0);
-    distancePerHalfMonth[basket] += Math.round(training.distance / 1000);
+    const bucket = (date.getMonth() * 3) + (monthPart(date.getDate()));
+    distancePerHalfMonth[bucket] += Math.round(training.distance / 1000);
   })
   return {
     "index": 0,
     "chartData": distancePerHalfMonth,
   };
+}
+
+function monthPart(dayOfMonth) {
+  if (dayOfMonth > 20) {return 2;}
+  if (dayOfMonth > 10) {return 1;}
+  return 0;
 }
